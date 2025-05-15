3bcb6190b088687caf81f14f43abd89b3c4d2572
SathvikaKamisetty <kamisetty.sathvika@accenture.com>
2025-05-15 15:18:16 +0530

    Update app.component.spec.ts

--- chatbot-app/chatbot-app/src/app/app.component.spec.ts
+++ chatbot-app/chatbot-app/src/app/app.component.spec.ts
@@ -34,6 +34,13 @@ describe('AppComponent', () => {
     component.addNumbers();
     expect(component.sum).toBe(70);
   });
+
+   it('should correctly add two positive numbers', () => {
+    component.num1 = 70;
+    component.num2 = 20;
+    component.addNumbers();
+    expect(component.sum).toBe(90);
+  });
   
 
 
