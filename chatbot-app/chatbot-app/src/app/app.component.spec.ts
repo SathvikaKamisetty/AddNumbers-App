5b39fa79c0a9a486cc9a00d9bc6adbc2b8a4c05f
Pranav-intern <p.murthy.ponnuswamy@accenture.com>
2025-05-15 12:37:49 +0530

    Update app.component.spec.ts

--- chatbot-app/chatbot-app/src/app/app.component.spec.ts
+++ chatbot-app/chatbot-app/src/app/app.component.spec.ts
@@ -70,6 +70,13 @@ describe('AppComponent', () => {
     expect(component.sum).toBe(30);
   });
 
+    it('should correctly add decimal numbers', () => {
+    component.num1 = 10;
+    component.num2 = 20;
+    component.addNumbers();
+    expect(component.sum).toBe(30);
+  });
+
   
 });
 
