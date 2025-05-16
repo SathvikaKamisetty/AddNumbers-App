ab1176e75953ad0fdc1318bcad5ab9898833f373
Pranav-intern <p.murthy.ponnuswamy@accenture.com>
2025-05-16 10:48:28 +0530

    Update app.component.spec.ts

--- chatbot-app/chatbot-app/src/app/app.component.spec.ts
+++ chatbot-app/chatbot-app/src/app/app.component.spec.ts
@@ -37,6 +37,13 @@ describe('AppComponent', () => {
     expect(component.sum).toBe(7);
   });
 
+   it('should correctly add numbers', () => {
+    component.num1 = 1;
+    component.num2 = 6;
+    component.addNumbers();
+    expect(component.sum).toBe(7);
+  });
+
  
 
   
