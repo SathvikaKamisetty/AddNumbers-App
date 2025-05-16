64753c52d4b5127eab0828eb965fa9d9206bc734
Pranav-intern <p.murthy.ponnuswamy@accenture.com>
2025-05-16 10:40:33 +0530

    Update chatbot.service.spec.ts

--- chatbot-app/chatbot-app/src/app/chatbot.service.spec.ts
+++ chatbot-app/chatbot-app/src/app/chatbot.service.spec.ts
@@ -13,4 +13,8 @@ describe('ChatbotService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 });
