32d40d10c48abe4d68f0207518529e2e1a779bb5
SathvikaKamisetty <kamisetty.sathvika@accenture.com>
2025-05-27 12:22:40 +0530

    Update AddControllerTests.cs

--- chatbot-app/addNumbersUsingC#/MyAPP/Tests/AddControllerTests.cs
+++ chatbot-app/addNumbersUsingC#/MyAPP/Tests/AddControllerTests.cs
@@ -77,6 +77,15 @@ namespace MyApp.Tests
             Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
         }
 
+
+        
+        [Test]
+        public async Task Add_InvalidInput_ReturnsBadRequest()
+        {
+            var response = await _client.GetAsync("/api/add?a=a1&b=3");
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
         
      }
 }
