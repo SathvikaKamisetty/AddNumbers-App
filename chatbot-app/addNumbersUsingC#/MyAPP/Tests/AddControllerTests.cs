a845a862a8ec1aeb033ad23ff36a26c91f2c2ef4
Pranav-intern <p.murthy.ponnuswamy@accenture.com>
2025-06-10 14:21:25 +0530

    Update AddControllerTests.cs

--- chatbot-app/addNumbersUsingC#/MyAPP/Tests/AddControllerTests.cs
+++ chatbot-app/addNumbersUsingC#/MyAPP/Tests/AddControllerTests.cs
@@ -47,6 +47,16 @@ namespace MyApp.Tests
             Assert.AreEqual("2", result);
         }
 
+        [Test]
+        public async Task Add_WithNegativeNumber_ReturnsCorrectSum()
+        {
+            var response = await _client.GetAsync("/api/add?a=4&b=-2");
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsStringAsync();
+            Assert.AreEqual("2", result);
+        }
+
        
         
      }
